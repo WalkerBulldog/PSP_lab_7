50c1475894697a3d09665c21ae38433c2ffb119c
snezhkalesha@gmail.com <snezhkalesha@gmail.com>
2022-11-18 08:54:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Sockets;
+using System.Net.WebSockets;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var webSocket = new WebSocket();
+        }
+    }
+}
